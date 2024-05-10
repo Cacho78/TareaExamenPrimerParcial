70f3bb58075487eafea956fab14346e3bfad80db
Cacho78 <147776832+Cacho78@users.noreply.github.com>
2024-05-10 07:34:22 -0400

    Endpoint Aumentados

--- rutas/bienvenidaruta.js
+++ rutas/bienvenidaruta.js
@@ -2,7 +2,7 @@ const express = require('express');
 const rutas = express.Router();
 const bienvenidaModel = require('../autos/bienvenida');
 
-//endpoint 1. traer todo
+//  EDPOINT 1. traer todo
 rutas.get('/getbienvenida', async (req, res) =>{
     try {
         const bienvenida = await bienvenidaModel.find();
@@ -15,7 +15,7 @@ rutas.get('/getbienvenida', async (req, res) =>{
     }
 })
 
-//endpoint 2. Crear
+//  EDPOINT 2. Crear
 rutas.post('/crear', async (req, res) => {
     const bienvenida = new bienvenidaModel({
     
@@ -31,34 +31,91 @@ rutas.post('/crear', async (req, res) => {
         const nuevabienvenida = await bienvenida.save();
         res.status(201).json(nuevabienvenida);
     } catch (error) {
-        res.status(400).json({ mensaje :  error.message})
+        res.status(400).json({ mensage :  error.message})
     }
 });
-//endpoint 3. Editar
+//  ENDPOINT 3. Editar
 rutas.put('/editar/:id', async (req, res) => {
     try {
         const recetabienvenida = await bienvenidaModel.findByIdAndUpdate(req.params.id, req.body, { new : true });
         if (!recetabienvenida)
-            return res.status(404).json({ mensaje : 'bienvenida no encontrada!!!'});
+            return res.status(404).json({ mensage : 'bienvenida no encontrada!!!'});
         else
             return res.status(201).json(bienvenidaEditada);
 
     } catch (error) {
-        res.status(400).json({ mensaje :  error.message})
+        res.status(400).json({ mensage :  error.message})
     }
 })
-//ENDPOINT 4. eliminar
+//  ENDPOINT 4. eliminar
 rutas.delete('/eliminar/:id',async (req, res) => {
     try {
        const bienvenidaEliminada = await bienvenidaModel.findByIdAndDelete(req.params.id);
        if (!bienvenidaEliminada)
             return res.status(404).json({ mensaje : 'bienvenida no encontrada!!!'});
        else 
-            return res.json({mensaje :  'bienvenida eliminada'});    
+            return res.json({mensage :  'bienvenida eliminada'});    
        } 
     catch (error) {
+        res.status(500).json({ mensage :  error.message})
+    }
+});
+
+// ENDPOINT 5. obtener una lista por su ID
+
+rutas.get('/lista/:id', async (req, res) => {
+    try {
+        const bienvenida = await bienvenidaModel.findById(req.params.id);
+        if (!bienvenida)
+            return res.status(404).json({ mensage : 'lista no encontrada!!!'});
+        else 
+            return res.json(bienvenida);
+    } catch(error) {
+        res.status(500).json({ mensage :  error.message})
+    }
+});
+ // ENDPOINT 6. obtener lista por un modelo especifico
+rutas.get('/bienvenidaPormodelo/:modelo', async (req, res) => {
+    try {
+        const bienbenidamodelos = await bienvenidaModel.find({ ingrediente: req.params.ingrediente});
+        return res.json(bienvenidamodelo);
+    } catch(error) {
         res.status(500).json({ mensaje :  error.message})
     }
 });
+// ENDPOINT 7 - eliminar todas las recetas
+rutas.delete('/eliminarTodos', async (req, res) => {
+    try {
+        await bienvenidaModel.deleteMany({});
+        return res.json({mensage: "Todas las listas han sido eliminadas"});
+    } catch(error) {
+        res.status(500).json({ mensage :  error.message})
+    }
+});
+// ENDPOINT 8. contar el numero total de recetas
+rutas.get('/totallistas', async (req, res) => {
+    try {
+        const total = await bienvenidaModel.countDocuments();
+        return res.json({totallistas: total });
+    } catch(error) {
+        res.status(500).json({ mensage :  error.message})
+    }
+});
+// ENDPOINT 9. obtener listas ordenadas por nombre ascendente
+// query.sort({ field: 'asc', test: -1 a 1 });
+rutas.get('/ordenarlistas', async (req, res) => {
+    try {
+       const listasOrdenadas = await bienvenidaModel.find().sort({ nombre: -1});
+       res.status(200).json(listasOrdenadas);
+    } catch(error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
+
+
+
+
+
+
 
 module.exports = rutas;
